d8d4461548563293f434b584847c9769cdd39c5d
Jess Green <jesgs@users.noreply.github.com>
2014-09-10 08:22:36 -0500

    Adding help text for Comic Page Options tab.

--- includes/pages/help-comic-page.php
+++ includes/pages/help-comic-page.php
@@ -5,3 +5,24 @@
  * @package help-comic-page
  * @author Jess Green <jgreen AT psy-dreamer.com>
  */
+ ?>
+<h3>Comic Page Options Help</h3>
+<p>
+    <?php _e('This section controls display of comic posts', MP_DOMAIN); ?>
+</p>
+
+<dl>
+    <dt><strong>Generate Comic Page</strong></dt>
+    <dd>
+      Generate a custom image size based on the values below. Use this if you wish to display slightly smaller images
+      than the image you've uploaded.
+    </dd>
+    <dt><strong>Comic Page Width</strong></dt>
+    <dd>
+      Set the width of your comic page image.
+    </dd>
+    <dt><strong>Comic Page Height</strong></dt>
+    <dd>
+      Set the height of you comic page image.
+    </dd>
+</dl>
