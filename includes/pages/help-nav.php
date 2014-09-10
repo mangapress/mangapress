236b146f8b1a802b2e54186a45cbf4a868a0fe96
Jess Green <jesgs@users.noreply.github.com>
2014-09-10 08:28:56 -0500

    Adding help tab for Navigation Options.

--- includes/pages/help-nav.php
+++ includes/pages/help-nav.php
@@ -5,3 +5,20 @@
  * @package help-nav
  * @author Jess Green <jgreen AT psy-dreamer.com>
  */
+ ?>
+<h3>Navigation Options Help</h3>
+<p>
+    This section handles the appearance of the navigation links for your comic.
+</p>
+
+<dl>
+    <dt><strong>Insert Navigation</strong></dt>
+    <dd>
+      When this option is enabled, comic navigation will automatically be inserted above the comic image. Not needed if you're
+      using one of the child-theme that ships with Manga+Press.
+    </dd>
+    <dt><strong>Navigation CSS</strong></dt>
+    <dd>
+      This option is turned off by default (Custom CSS). Set to Default CSS to use the included styles if your theme doesn't provide styles for the links.
+    </dd>
+</dl>
