5154c4cd0dd372c7d333091babbe6fbc6e16f36b
Jess Green <jgreen@psy-dreamer.com>
2014-12-14 05:57:04 +0000

    Updated text domain.

--- includes/lib/class-mp-calendar-widget.php
+++ includes/lib/class-mp-calendar-widget.php
@@ -22,10 +22,10 @@ class MangaPress_Widget_Calendar extends WP_Widget
     {
         $widget_ops = array(
             'classname' => 'mangapress_widget_calendar',
-            'description' => __( 'A calendar of your site&#8217;s archived Comic Posts.')
+            'description' => __( 'A calendar of your site&#8217;s archived Comic Posts.', MP_DOMAIN)
         );
 
-        parent::__construct('mangapress_calendar', __('Manga+Press Calendar'), $widget_ops);
+        parent::__construct('mangapress_calendar', __('Manga+Press Calendar', MP_DOMAIN), $widget_ops);
     }
 
 
@@ -69,7 +69,7 @@ class MangaPress_Widget_Calendar extends WP_Widget
         $instance = wp_parse_args( (array) $instance, array( 'title' => '' ) );
         $title = strip_tags($instance['title']);
         ?>
-        <p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+        <p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', MP_DOMAIN); ?></label>
         <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" /></p>
         <?php
     }
