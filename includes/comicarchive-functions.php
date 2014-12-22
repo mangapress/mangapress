e5df0c17ea93a3d143c15f97123e5eb11250555e
Jess Green <jgreen@psy-dreamer.com>
2014-12-22 02:26:16 +0000

    Correcting problem with archive page queries

--- includes/comicarchive-functions.php
+++ includes/comicarchive-functions.php
@@ -17,13 +17,12 @@
 function mangapress_get_all_comics_for_archive()
 {
     global $wp_actions;
+
     // TODO Ordering options
     // TODO Sort parameters for taxonomies
     // TODO Move add/remove filter calls to separate functions
     do_action('_mangapress_pre_archives_get_posts');
-    add_filter('posts_orderby', 'mangapress_orderby');
-    add_filter('posts_fields', 'mangapress_select_fields');
-    add_filter('posts_join', 'mangapress_join');
+
     $archives = new WP_Query(array(
         'post_type'       => 'mangapress_comic',
         'posts_per_page'  => -1,
@@ -32,10 +31,8 @@ function mangapress_get_all_comics_for_archive()
     if (isset($wp_actions['_mangapress_pre_archives_get_posts'])) {
         unset($wp_actions['_mangapress_pre_archives_get_posts']);
     }
+
     remove_all_actions('_mangapress_pre_archives_get_posts');
-    remove_filter('posts_orderby', 'mangapress_orderby');
-    remove_filter('posts_fields', 'mangapress_select_fields');
-    remove_filter('posts_join', 'mangapress_join');
 
     return $archives;
 }
