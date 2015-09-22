ae062033aea7f185e52e9c2709d19d477928e601
Jess Green <jgreen@psy-dreamer.com>
2015-09-22 21:19:39 +0000

    Task #27 Adding bookmarking history markup.

--- assets/js/bookmark.js
+++ assets/js/bookmark.js
@@ -14,8 +14,10 @@
             Bookmark.bookmark();
         });
 
-        $('#show-comic-bookmark-history').on('click', function (e) {
+        $('#bookmark-comic-history').on('click', function (e) {
             // show a list of recently bookmarked comics, starting with most recent
+            e.preventDefault();
+            Bookmark.history();
         });
 
     });
@@ -25,6 +27,7 @@
         BOOKMARK_HISTORY : 'mangapress-bookmark-history',
         BOOKMARK : 'mangapress-bookmark',
         $bookmark : null,
+        $bookmarkNav : null,
         init: function() {
             this.storage = localStorage;
             this.checkItem();
@@ -35,6 +38,7 @@
                 pageHref = window.location.href;
 
             this.$bookmark = $('#bookmark-comic');
+            this.$bookmarkNav = $('#comic-bookmark-navigation');
 
             if (bookmark == null) {
                 return;
@@ -46,8 +50,8 @@
         },
 
         bookmark : function() {
-            var href = window.location.href,
-                pageTitle = window.document.title,
+            var href = (this.$bookmark.data('href') !== undefined) ? this.$bookmark.data('href') : window.location.href,
+                pageTitle = (this.$bookmark.data('title') !== undefined) ? this.$bookmark.data('title') : window.document.title,
                 data = {};
 
             var existingBookmarkData = this.storage.getItem(this.BOOKMARK);
@@ -75,7 +79,49 @@
         },
 
         history : function() {
+            var self = this,
+                revBookmarkHistory = JSON.parse(self.storage.getItem(self.BOOKMARK_HISTORY)),
+                bookmarkHistory = revBookmarkHistory.reverse(),
+                $historyModal = $('<div id="bookmark-history-modal"><div id="bookmark-history-content"></div><p>[<a href="#" id="bookmark-history-close">close</a>]</p></div>').css({
+                    'width': '300px',
+                    'z-index' : 9999,
+                    'border' : '1px solid black',
+                    'background-color' : '#fff',
+                    'position' : 'absolute',
+                    'padding' : '5px',
+                    // 'top' : '25%',
+                    'left' : '50%',
+                    'margin-left' : '-150px'
+                });
 
+                $historyModal.find('#bookmark-history-content').html(function(){
+                    var htmlString = "<table>";
+
+                    htmlString = "<thead><tr><td>Title</td><td>Date</td></tr></thead>";
+
+                    for (var i = 0; i < bookmarkHistory.length; i++) {
+                        var columns = [],
+                            bookmark = JSON.parse(bookmarkHistory[i]),
+                            d = new Date(bookmark.date),
+                            date = d.getMonth() + '/' + d.getDate() + '/' + d.getFullYear(),
+                            link = "<a href=\"" + bookmark.url + "\">" + bookmark.title + "</a>";
+
+                        columns.push(link, date);
+
+                        htmlString += "<tr><td>" + columns.join('</td><td>') + "</td></tr>"
+                    }
+
+                    return htmlString + "</table>";
+                });
+
+                // append
+                self.$bookmarkNav.append($historyModal)
+
+                // add event for closing modal
+                $('#bookmark-history-close').one('click', function(e){
+                    e.preventDefault();
+                    $historyModal.remove();
+                });
         },
 
         goto : function() {
