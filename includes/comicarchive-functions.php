abb4e30ed7074613c31e25f9089f271aceb1dd81
Jess Green <ardath.ksheyna78@gmail.com>
2016-01-22 10:14:05 -0600

    Updating theme templates to use new sorting order parameters

--- includes/comicarchive-functions.php
+++ includes/comicarchive-functions.php
@@ -14,7 +14,7 @@
  * @since 2.9
  * @return \WP_Query
  */
-function mangapress_get_all_comics_for_archive()
+function mangapress_get_all_comics_for_archive($params = array())
 {
     global $wp_actions;
 
@@ -22,14 +22,23 @@ function mangapress_get_all_comics_for_archive()
     // TODO Sort parameters for taxonomies
     // TODO Move add/remove filter calls to separate functions
     do_action('_mangapress_pre_archives_get_posts');
-    $mp_options = MangaPress_Bootstrap::get_instance()->get_options();
 
-    $archives = new WP_Query(array(
-        'post_type'       => 'mangapress_comic',
-        'posts_per_page'  => -1,
-        'order'         => $mp_options['basic']['archive_order'],
-        'orderby'         => $mp_options['basic']['archive_orderby']
-    ));
+    $mp_options = MangaPress_Bootstrap::get_instance()->get_options();
+    $order_params = array(
+        'order' => $mp_options['basic']['archive_order'],
+        'orderby' => $mp_options['basic']['archive_orderby'],
+    );
+
+    if (empty($params)) {
+        $params = array(
+            'post_type'      => 'mangapress_comic',
+            'posts_per_page' => -1,
+            'post_status'    => 'publish'
+        );
+    }
+    $params = array_merge($params, $order_params);
+
+    $archives = new WP_Query($params);
 
     if (isset($wp_actions['_mangapress_pre_archives_get_posts'])) {
         unset($wp_actions['_mangapress_pre_archives_get_posts']);
