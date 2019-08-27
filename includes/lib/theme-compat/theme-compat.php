c1c32319590b5ea3390b81e2211267cc9e1e0b20
Jess Green <2304490+jesgs@users.noreply.github.com>
2019-08-27 08:09:30 -0500

    Moving code around.

--- includes/lib/theme-compat/theme-compat.php
+++ includes/lib/theme-compat/theme-compat.php
@@ -11,25 +11,25 @@ class ThemeCompatibility
      * Initialize class
      */
     public static function init()
-    {
+    {        
+        require_once MP_ABSPATH . 'includes/lib/theme-compat/ThemeMarkup.php';
+        
         $theme = get_template();
         $theme_compat_path = implode('/', ['includes', 'lib', 'theme-compat']) . '/';
         $theme_compat_file = MP_ABSPATH . $theme_compat_path . $theme . '.php';
 
         if ( ! file_exists( $theme_compat_file )) {
-            do_action("mangapress_theme_compatible-{$theme}");
-        } else {
-            require_once MP_ABSPATH . 'includes/lib/theme-compat/ThemeMarkup.php';
             /**
              * mangapress_theme_compatible-$theme
              *
              * Allow third-party themes to declare Manga+Press compatibility options.
              *
              * @since 4.0.0
-             */
+             */            
+            do_action("mangapress_theme_compatible-{$theme}");
+        } else {
             require_once $theme_compat_file;
         }
-
     }
 }
 
