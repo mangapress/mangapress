e8b2ebb93c937d37ea8d4e00853827536398b332
jesgs <2304490+jesgs@users.noreply.github.com>
2019-09-24 20:44:16 -0500

    Add options class

--- src/Options/Options.php
+++ src/Options/Options.php
@@ -9,5 +9,48 @@ namespace MangaPress\Options;
  */
 class Options
 {
+    /**
+     * Options array
+     * @var array $options
+     */
+    protected static $options;
 
+    /**
+     * Default options—used on initial plugin activation
+     * @var array
+     */
+    protected static $default_options;
+
+    /**
+     * Initialize options
+     */
+    public function init()
+    {
+        self::$options = maybe_unserialize(get_option());
+    }
+
+    /**
+     * Get all currently set options
+     * @return array
+     */
+    public static function get_options()
+    {
+        return self::$options;
+    }
+
+    /**
+     * Get option value
+     *
+     * @param string $name
+     * @param string $section
+     * @return mixed
+     */
+    public static function get_option($name, $section)
+    {
+        if (isset(self::$options[$section][$name])) {
+            return self::$options[$section][$name];
+        }
+
+        return self::$default_options[$section][$name];
+    }
 }
