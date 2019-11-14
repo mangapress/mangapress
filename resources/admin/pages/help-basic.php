26f4ed0aeda5439278e18a8c023fe46163cb95a0
jesgs <2304490+jesgs@users.noreply.github.com>
2019-11-13 22:02:42 -0600

    Updating POT and po files

--- resources/admin/pages/help-basic.php
+++ resources/admin/pages/help-basic.php
@@ -22,13 +22,12 @@
     </dd>
     <dt><strong><?php _e('Comic Archive Page Style', MP_DOMAIN); ?></strong></dt>
     <dd>
-        <?php _e('Choose an archive page style from the drop-down. This option can be overridden by adding a <code>archive-*.php</code> template to your theme inside the <code>comics</code> sub-directory.', MP_DOMAIN); ?>
-        <?php _e('Available templates are: <code>archive-list.php</code>, <code>archive-gallery.php</code>, and <code>archive-calendar.php</code>', MP_DOMAIN); ?>
+        <?php _e('Choose an archive page style from the drop-down.', MP_DOMAIN); ?>
     </dd>
     <dt><strong><?php _e('Latest Comic Page and Comic Archive Page', MP_DOMAIN); ?></strong></dt>
     <dd>
         <p>
-            <?php _e('Templates for Latest Comic Page and Comic Archive Page are (respectively) <code>latest-comic.php</code> and <code>archive-comic.php</code>.'); ?>
+            <?php _e('Templates for Latest Comic Page and Comic Archive Page are (respectively) <code>page-latest-comic.php</code> and <code>archive-comic.php</code>.'); ?>
             <?php _e('These templates can be overridden by placing them in your theme directory.'); ?>
         </p>
     </dd>
