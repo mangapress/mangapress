1e1715e26e04d99c04a9ea61a6f46481860696df
Jess G <2304490+jesgs@users.noreply.github.com>
2026-05-29 17:41:15 -0500

    Fix PHP warnings appearing in debug.log

--- class-mangapress-options.php
+++ class-mangapress-options.php
@@ -432,7 +432,7 @@ final class MangaPress_Options {
 				$new_options['nav']['nav_css'] = 'default_css';
 			}
 
-			$new_options['nav']['enable_random_link'] = boolval( $options['nav']['enable_random_link'] );
+			$new_options['nav']['enable_random_link'] = boolval( $options['nav']['enable_random_link'] ?? false );
 		}
 
 		if ( 'basic' === $section ) {
