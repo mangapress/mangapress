a10a1adf0970b6120cbc430edc19e4e85afd8a94
DAnn2012 <dannunziata@hotmail.it>
2026-06-28 12:14:14 +0200

    Update help-basic.php

--- src/pages/help-basic.php
+++ src/pages/help-basic.php
@@ -29,7 +29,7 @@
 	<dd>
 		<?php esc_html_e( 'Select an available page from the drop-down to use as your Comic Archive page. This page displays all comics in chronological order, starting with the most recent.', 'mangapress' ); ?>
 	</dd>
-	<dt><strong><?php echo esc_html_e( 'Comic Archive Page Style', 'mangapress' ); ?></strong></dt>
+	<dt><strong><?php esc_html_e( 'Comic Archive Page Style', 'mangapress' ); ?></strong></dt>
 	<dd>
 		<?php esc_html_e( 'Select an archive page style from the drop-down. This option can be overridden by adding a comic-archive-*.php template to your theme inside the <code>comics</code> sub-directory.', 'mangapress' ); ?>
 		<?php esc_html_e( 'Available templates are: comic-archive-list.php, comic-archive-calendar.php, and comic-archive-gallery.php', 'mangapress' ); ?>
